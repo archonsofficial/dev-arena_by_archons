abed9be27a86396966e56dac789ebaf549386d4b
Mayank Gupta <mayankg88813@gmail.com>
2024-08-02 09:03:34 +0530

    new changes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App';
-
+import App from './App';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './index.css';
 
-// import Home from './Components/Home/Home_page';
+import Home from './Components/Home/Home_page';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   <React.StrictMode>
-   
-   {/* <App/> */}
+   <Router>
+      <Routes>
+        <Route path="/" element={<App/>} />
+        <Route path="/home" element={<Home />} />
+        {/* You can add more routes here as needed */}
+      </Routes>
+      {/* Common components like Footer can stay outside the Routes to be always rendered */}
+      {/* <Footer /> */}
+    </Router>
    
      {/* <Home/> */}
      
   </React.StrictMode>
 );
-
